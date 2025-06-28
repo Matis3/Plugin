b1136395164c2956e07098fab5c6971cd9318a60
Mattia <mattia.tamborrini@proton.me>
2025-06-28 17:24:11 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,6 @@
 # install neofetch on windows 
 winget install --id=nepnep.neofetch-win  -e
+# Download The textfile in this repo
+
 # use the neofetch command with custom image
 neofetch --ascii /neofetch/iwfcom.txt
